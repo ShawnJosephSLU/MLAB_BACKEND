4454ee39f5e24602e0a9071b4101a0464a688b3b
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2023-12-11 10:27:18 -0400

    make the default price 0.00

--- models/product.model.js
+++ models/product.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, unique: true, required: true },
-    price: {type: Number, required: true},
+    price: {type: Number, required: true, default: 0.00},
     version: {
         major: { type: Number, default: 1 },
         minor: { type: Number, default: 0 },
