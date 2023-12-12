80530066a45da16edf44c12614559b2a36540f78
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2023-12-12 17:48:17 -0400

    Update Root route welcome message

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next)=>{
 
 // Root route
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to MLAB SERVICES');
+    res.status(200).send('Welcome to MLAB SERVICES API');
 });
 
 const userRoutes = require('./routes/users');
