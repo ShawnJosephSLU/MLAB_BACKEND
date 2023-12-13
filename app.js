c8c2550cd6f97fb1875e69cece6f41359d33574d
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2023-12-12 23:56:34 -0400

    create error handler middleware

--- app.js
+++ app.js
@@ -47,24 +47,11 @@ app.use('/admin', adminRoutes);
 
 
 const productRoutes = require('./routes/products');
+const { notFound, errorHandler } = require('./middleware/error-handler');
 app.use('/products', productRoutes);
 
-
 // error handling
-app.use((req, res, next) =>{
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-});
-
-app.use((error, req, res, next) =>{
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-
-});
+app.use(notFound);
+app.use(errorHandler); 
 
 module.exports = app;
